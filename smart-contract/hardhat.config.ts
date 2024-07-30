65918cfd5fc1580863295b8a2ca9f30aeac1b8fa
Dean <o.michaeldean@gmail.com>
2024-07-30 16:46:57 +0100

    project init

--- smart-contract/hardhat.config.ts
+++ smart-contract/hardhat.config.ts
@@ -3,18 +3,17 @@ import "@nomicfoundation/hardhat-toolbox";
 import "dotenv/config";
 import { vars } from "hardhat/config";
 
-const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
 
 const config: HardhatUserConfig = {
  solidity: "0.8.20",
  networks: {
     sepolia: {
-      url: process.env.URL,
-      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      url: vars.get("SEPOLIA_URL"),
+      accounts: [`0x${vars.get("PRIVATE_KEY")}`],
     },
  },
  etherscan: {
-    apiKey: ETHERSCAN_API_KEY
+    apiKey: vars.get("ETHERSCAN_API_KEY")
  },
 };
 
